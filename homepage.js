e548935e5a2cd6ed5c65e76441291e98f0ed1e77
Progdee <dhebhyofummite1@yahoo.com>
2023-01-20 13:29:54 +0100

    my first website

---
+++ homepage.js
@@ -0,0 +1,16 @@
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+
+
+function create() {
+    let circle1 = this.add.circle(50, 100, 90, 0xFFF070;)
+}
+const config = {
+    type: Phaser.AUTO,
+    width: 450,
+    height: 600,
+    scene: {
+        create
+    }
+}
